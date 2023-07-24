7b007cf664cac05c94aad644540688c063dfc6bf
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-07-23 23:17:42 -0400

    updated userPrompt file with .then commands for each choice a user could make, user choice will then call that specific function.

--- db/Javascripts/db.js
+++ db/Javascripts/db.js
@@ -9,9 +9,7 @@ const dbconnection = mysql.createConnection(
       user: 'root',
       password: process.env.DB_PASS,
       database: 'employee_db'
-    },
-    console.log(`Connected to the employee database.`)
-  );
+    });
   
   const runQuery = (sql, values) => {
       return new Promise((resolve, reject) => {
--- db/Javascripts/userPrompt.js
+++ db/Javascripts/userPrompt.js
@@ -26,6 +26,42 @@ const userPrompt = () => {
             ]
         }
     ])
+
+    .then((answers) => {
+        const { choices } = answers;
+
+        if (choices === 'View All Departments') {
+            showDepartments();
+        } else if (choices === 'View All Roles') {
+            showRoles();
+        } else if (choices === 'View All Employees') {
+            showEmployees();
+        } else if (choices === 'View Employees by Manager') {
+            managersRoster();
+        } else if (choices === 'View Employees by Department') {
+            departmentsRoster();
+        } else if (choices === 'View A Department Budget') {
+            depBudget();
+        } else if (choices === 'Add Department') {
+            depBudget();
+        } else if (choices === 'Add Role') {
+            depBudget();
+        } else if (choices === 'Add Employee') {
+            depBudget();
+        } else if (choices === 'Update Employee Role') {
+            depBudget();
+        } else if (choices === 'Update Employee Manager') {
+            depBudget();
+        } else if (choices === 'Delete Department') {
+            depBudget();
+        } else if (choices === 'Delete Role') {
+            depBudget();
+        } else if (choices === 'Delete Employee') {
+            depBudget();
+        } else if (choices === 'Exit') {
+            depBudget();
+        }
+    })
 }
 
 module.exports = {
