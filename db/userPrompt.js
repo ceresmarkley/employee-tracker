57117ac880b2730f3fcd11e240b11a00e3207bfc
markley man <129554518+ceresmarkley@users.noreply.github.com>
2023-07-23 22:12:54 -0400

    in userPrompt.js file created const require connections to server.js as well as inquirer. also created initial inq prompt messages.

---
+++ db/userPrompt.js
@@ -0,0 +1,29 @@
+const { runQuery, dbconnection } = require('./db');
+const inquirer = require('inquirer');
+
+const userPrompt = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choices',
+            message: "Hello User! What would you like to do?",
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'View Employees by Manager',
+                'View Employees by Department',
+                'View A Department Budget',
+                'Add Department',
+                'Add Role',
+                'Add Employee',
+                'Update Employee Role',
+                'Update Employee Manager',
+                'Delete Department',
+                'Delete Role',
+                'Delete Employee',
+                'Exit'
+            ]
+        }
+    ])
+}
--- server.js
+++ server.js
@@ -13,12 +13,22 @@ const dbconnection = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
-    password: process.end.DB_PASS,
+    password: process.env.DB_PASS,
     database: 'employee_db'
   },
   console.log(`Connected to the employee database.`)
 );
 
+const runQuery = (sql, values) => {
+    return new Promise((resolve, reject) => {
+        connection.query(sql, values, (err, results) => {
+            if (err) return reject(err);
+            resolve(results);
+        });
+    });
+};
+
 module.exports = {
     dbconnection,
+    runQuery,
 }
